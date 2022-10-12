675d7e359d1af349eadf34eba424299676b9d57d
nkrell <98400970+nkrell@users.noreply.github.com>
2022-10-12 09:29:30 -0400

    hi

--- FastaSequence.java
+++ FastaSequence.java
@@ -8,6 +8,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.TreeMap;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FastaSequence
 {
@@ -43,14 +46,45 @@ public class FastaSequence
 		return(Float(counter/sequence.length()));
 	}
 
-	public static List<FastaSequence> readFastaFile(String filepath) throws Exception
+	public static List<FastaSequence> readFastaFile(File file) throws Exception
 	{
-
+		List<FastaSequence> list = new ArrayList<FastaSequence>();
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String nextLine = reader.readLine();
+		return(list);
 	}
 
 	//public static void writeTableSummary( List<FastaSequence> list, File outputFile) throws Exception
 	//{
 
 	//}
+
+}
+//ExaMPLE
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFastaFile {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        boolean first = true;
+
+        try (Scanner sc = new Scanner(new File("test.fasta"))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                if (line.charAt(0) == '>') {
+                    if (first)
+                        first = false;
+                    else
+                        System.out.println();
+                    System.out.printf("%s: ", line.substring(1));
+                } else {
+                    System.out.print(line);
+                }
+            }
+        }
+        System.out.println();
+    }
 
 }
