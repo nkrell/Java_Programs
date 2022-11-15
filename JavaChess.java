da82c33102f399d651fd09853cc3c4185096bc31
nkrell <98400970+nkrell@users.noreply.github.com>
2022-11-15 14:11:03 -0500

    Update

---
+++ JavaChess.java
@@ -0,0 +1,29 @@
+
+
+public class JavaChess
+{
+	private abstract class LinearPiece
+	{
+		//
+	}
+
+	private abstract class NonLinearPiece
+	{
+		//
+	}
+
+	public static void main(String[] args)
+	{
+		//
+	}
+
+	private class Board 
+	{
+		//
+	}
+
+	private class Space
+	{
+		//
+	}
+}
